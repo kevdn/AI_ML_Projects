6ce8af375e865b1713db3465fe49e26163ee1c25
Khoa Le <53362871+kevdn@users.noreply.github.com>
2022-12-29 23:34:00 +0700

    Update streamlit_app.py

--- Basic_LinearRegression/streamlit_app.py
+++ Basic_LinearRegression/streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-file = st.file.uploader("Choose a file", type=[".csv"])
+file = st.file_uploader("Choose a file", type=[".csv"])
 if file is not None:
     st.write("The size of the file is", file.size)
