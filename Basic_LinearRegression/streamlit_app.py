2be967bd69f498f7a6ecbd9b5c04af0d5b6ae69a
Khoa Le <53362871+kevdn@users.noreply.github.com>
2022-12-30 01:12:30 +0700

    Update streamlit_app.py

--- Basic_LinearRegression/streamlit_app.py
+++ Basic_LinearRegression/streamlit_app.py
@@ -2,8 +2,20 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-chart_data = pd.DataFrame(
-    np.random.randn(20, 3),
-    columns=["A", "B", "C"])
+st.markdown("### Upload your `.csv` file here")
+uploaded_file = st.file_uploader("Choose a file", type=[".csv"])
 
-st.bar_chart(chart_data)
+if uploaded_file is not None:
+    #LOAD AND SPLIT DATA
+    df = pd.read_csv(uploaded_file)
+    number = st.number_input('Insert test size')
+    st.write('The test size is ', number)
+    X = dataset.iloc[ : ,   : 1 ].values
+    Y = dataset.iloc[ : , 1 ].values
+    X_train, X_test, Y_train, Y_test = train_test_split( X, Y, test_size = 1/4, random_state = 0) 
+
+    st.markdown("### Your uploaded data")
+    st.write(df)
+
+    
+   
