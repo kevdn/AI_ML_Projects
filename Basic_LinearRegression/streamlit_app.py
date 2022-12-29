a3e5c00b92da756eafc4fe2cc3041e8f82d2e6b6
Khoa Le <53362871+kevdn@users.noreply.github.com>
2022-12-30 00:27:14 +0700

    Update streamlit_app.py

--- Basic_LinearRegression/streamlit_app.py
+++ Basic_LinearRegression/streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-file = st.file_uploader("Choose a file", type=[".csv"])
-if file is not None:
-    st.write("The size of the file is", file.size)
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=["A", "B", "C"]),
+)
+st.line_chart(chart_data)
