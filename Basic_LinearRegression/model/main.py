bd5f889734004dd2dbe87572639230912854c928
Khoa Le <53362871+kevdn@users.noreply.github.com>
2022-12-29 18:50:03 +0700

    Update main.py

--- Basic_LinearRegression/model/main.py
+++ Basic_LinearRegression/model/main.py
@@ -7,7 +7,7 @@ from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-     X_train, X_test, Y_train, Y_test = load_data()
+     X_train, X_test, Y_train, Y_test = load_data(Basic_LinearRegression/data/studentscores.csv)
      train(X_train, Y_train)
      Y_pred = predict(X_test, X_train, Y_train)
      print(Y_pred)
