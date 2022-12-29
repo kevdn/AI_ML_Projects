37b0292538fc84b710b828da1cd01a1c4552df7e
Kev <lehuukhoa12@gmail.com>
2022-12-29 17:17:24 +0700

    delete files

--- predict.py
+++
@@ -1,7 +0,0 @@
-from sklearn.linear_model import LinearRegression 
-
-def predict(X_test, X_train, Y_train):
-    regressor = LinearRegression()
-    regressor = regressor.fit(X_train, Y_train)
-    Y_pred = regressor.predict(X_test)
-    return Y_pred
